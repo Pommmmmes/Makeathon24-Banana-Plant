69564b7cd4e976d871ba38501cc08d4cf12a46ae
qnert <qnertsimon@gmail.com>
2024-02-28 19:25:59 +0000

    simple flask server with databse that is not used yet

--- server/main.py
+++ server/main.py
@@ -1,6 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+import sqlite_utils
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=["GET"])
+def hello():
+    return render_template('hello.html')
+
+@app.route('/recieve', methods=["POST"])
+def process_data():
+    if request.is_json:
+        json_data = request.json
+        try:
+            return json_data["time"]
+        except KeyError:
+            return "Wrong data json file"
+    else:
+        return jsonify({"error": "No JSON data received"}), 400
+
+
+
+def main():
+    sqlite_utils.initialize_db()
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+main()
+
+# def initialize_db(
+# def initialize_db():
+# add_row(uuid: str, id: str, timestamp: int , temp: int,
+            # humidity: int, red: int, green: int, blue: int):
+# def delete_row(row_uuid):
+
+# add_row(uuid: str, id: str, timestamp: int , temp: int,
+            # humidity: int, red: int, green: int, blue: int):
+# def delete_row(row_uuid):
---
+++ server/sqlite_utils.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+def initialize_db():
+    conn = sqlite3.connect('measurements.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS measurements
+                (uuid TEXT PRIMARY KEY NOT NULL,
+				id TEXT,
+                temperature INTEGER,
+                humidity INTEGER,
+                timestamp INTEGER,
+              	red INTEGER,
+              	green INTEGER,
+              	blue INTEGER);''')
+    conn.commit()
+    conn.close()
+
+
+def add_row(uuid: str, id: str, timestamp: int , temp: int,
+            humidity: int, red: int, green: int, blue: int):
+    conn = sqlite3.connect('measurements.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO measurements (uuid, banana_id, temperature, humidity, timestamp, red, green, blue) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+              (uuid, id, temp, humidity, timestamp, red, green, blue))
+    conn.commit()
+    conn.close()
+
+
+def delete_row(row_uuid):
+    conn = sqlite3.connect('measurements.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM measurements WHERE uuid = ?", (row_uuid,))
+    conn.commit()
+    conn.close()
+
+# tbd ??
+
+# def fetch_row_by_uuid(row_uuid):
+#     conn = sqlite3.connect('measurements.db')
+#     c = conn.cursor()
+#     c.execute("SELECT status, content FROM users WHERE uuid = ?", (row_uuid,))
+#     row = c.fetchone()
+#     conn.close()
+#     if row is None:
+#         return None  # Return None if no row is found
+#     else:
+#         return int(row[0]), row[1]  # Return status and content as a dictionary
+
+
+# def update_single_column(row_uuid, column_name, new_value):
+#     conn = sqlite3.connect('generations.db')
+#     c = conn.cursor()
+#     # Use parameterized query to update the specified column
+#     query = f"UPDATE users SET {column_name} = ? WHERE uuid = ?"
+#     c.execute(query, (new_value, row_uuid))
+#     conn.commit()
+#     conn.close()
